9298068174dd0570f981d5060b03c4c0daa4d4cd
Tanya <82596524+tanya247@users.noreply.github.com>
2021-05-05 13:54:12 +0530

    UC1 SHOWED

--- src/test/java/FindMaximum/IntegerMaximumTestTC.java
+++ src/test/java/FindMaximum/IntegerMaximumTestTC.java
@@ -7,17 +7,17 @@ public class IntegerMaximumTestTC {
 	IntegerMaximum IM = new IntegerMaximum();
 	@Test
 	 public void givenMaximumIntegerAtPosition1_ReturnMaximumIntegerOfPosition1_TC1() {
-		Integer result= IM.maximum(3,4,5);
-		Assert.assertSame(5, result);
+		Integer result= IM.maximum(7,5,1);
+		Assert.assertSame(7, result);
 	 }
 	 @Test
 	 public void givenMaximumIntegerAtPosition2_ReturnMaximumIntegerOfPosition2_TC2() {
-		 Integer result=IM.maximum(3,5,4);
-	     Assert.assertSame(5,result);
+		 Integer result=IM.maximum(1,15,4);
+	     Assert.assertSame(15,result);
 	 }
 	 @Test
-	 public void givenMaximumIntegerAtPosition3_ShouldReturnIntegerOfPosition3() {
-		 Integer result= IM.maximum(5,3,4);
-	     Assert.assertSame(5,result);
+	 public void givenMaximumIntegerAtPosition3_ReturnMaximumIntegerOfPosition2_TC3() {
+		 Integer result= IM.maximum(15,3,40);
+	     Assert.assertSame(40,result);
 	 }
 }
